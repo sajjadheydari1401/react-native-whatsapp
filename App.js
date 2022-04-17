5b0d6880427d81e5a6f588e331915d7a990df5fd
Sajjad Heydari <sajjadheydari1401@gmail.com>
2022-04-17 15:56:23 +0430

    Added some context config

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Text, View } from "react-native";
+import { useAssets } from "expo-asset";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import SignIn from "./screens/SignIn";
+import ContextWrapper from "./context/ContextWrapper";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+export function App() {
+  const [currUser, setCurrUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      if (user) {
+        setCurrUser(user);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {!currUser ? (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="signIn" component={SignIn} />
+        </Stack.Navigator>
+      ) : (
+        <View>Hello Simon!</View>
+      )}
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Main() {
+  const [assets] = useAssets(
+    require("./assets/icon-square.png"),
+    require("./assets/chatbg.png"),
+    require("./assets/user-icon.png"),
+    require("./assets/welcome-img.png")
+  );
+  if (!assets) {
+    return <Text>Loading ..</Text>;
+  }
+  return (
+    <ContextWrapper>
+      <App />
+    </ContextWrapper>
+  );
+}
+
+export default Main;
